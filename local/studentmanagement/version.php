b800878c3c574605b1de223aecbc6a2d6fd740ef
Angad Sharma <angadsharma@192.168.0.103>
2026-05-03 19:49:44 +0530

    modify studentmanagement

--- local/studentmanagement/version.php
+++ local/studentmanagement/version.php
@@ -3,5 +3,5 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_studentmanagement';
 
-$plugin->version = 2026041904;
+$plugin->version = 2026041905;
 $plugin->requires = 2022041900;
